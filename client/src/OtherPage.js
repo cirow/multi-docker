6e7b86c02d01e7d40b3736fcd1942cdb10d666c6
ciro <ciro.perezjr@gmail.com>
2020-06-06 15:43:36 -0300

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      The dark side of the page <br/>
+      <Link to="/">Take me hoooome!</Link>
+    </div>
+  );
+};
